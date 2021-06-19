9f8b9f19a808ef2a79c061939f992bc0a4f29e2b
Daniel Hedrick <misterhedrick@gmail.com>
2021-06-18 21:59:04 -0400

    updates to the data to match resume

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,38 +5,40 @@
     (4, 'HTML(5)', ''),
     (5, 'Java(8)', ''),
     (6, 'Javascript', ''),
-    (7, 'Nativescript', ''),
-    (8, '.Net Core', ''),
-    (9, 'SQL', '');
+    (7, 'jQuery', ''),
+    (8, 'SQL', '');
 
     INSERT INTO achievement (id, description) VALUES
     (1, 'Lead Angular POC'),
     (2, 'Lead Raspberry Pi POC'),
-    (3, 'Held multiple lunch and learns teaching IT dept basic Angular (150+ people)'),
+    (3, 'Held multiple lunch and learns teaching IT dept basic Angular'),
     (4, 'Developed CMS for website'),
-    (5, 'Migrate website from JSP to JSF'),
-    (6, 'Migrated multiple applications from Green Screen (AS400) to Java / HTML');
+    (5, 'Migrate website from JSP to JSF');
 
     INSERT INTO experience (id, dates, title, employer) VALUES
     (1, 'June 2019 - Present', 'Software Engineer', 'CoreLogic'),
-    (2, 'Sept 2017 - June 2019', 'Senior Front-End Developer', 'Old Dominion Freight Line'),
+    (2, 'Sept 2017 - June 2019', 'Senior UI/UX Developer', 'Old Dominion Freight Line'),
     (3, 'Feb 2012 - Sept 2017', 'Full Stack Developer', 'Old Dominion Freight Line'),
     (4, 'Mar 2010 - Feb 2012', 'Edi Specialist', 'Old Dominion Freight Line');
 
     INSERT INTO highlight (id, experience, description) VALUES
-    (1, 1, 'Building responsive front ends using Angular connecting to .Net Core and Springboot Endpoints'),
-    (2, 1, 'Building rest API’s using .Net Core and Springboot'),
-    (3, 1, 'Leading a POC working with Raspberry Pi’s and creating Google Assistant Commands to learn Ping Pong'),
-    (4, 2, 'Building responsive front ends using CSS Frameworks such as Foundation and Bootstrap'),
-    (5, 2, 'Collaborated with diverse programming teams to prototype and solve complex business problems'),
-    (6, 2, 'Created containerized Docker front end UIs'),
-    (7, 2, 'Developed Angular Front-end for the application using Angular controllers, partial views and routers and services'),
-    (8, 2, 'Created responsive user interfaces that engage customers across different devices and resolutions'),
-    (9, 3,  'Designed and developed a web based toolset to create, review, and analyze freight shipments using J2EE'),
-    (10, 3, 'Production support including analyzing and repairing defects'),
-    (11, 3, 'Developed, tested, and designed software using Agile Scrum methodologies'),
-    (12, 3, 'Helped transition external website from JSP/WebSphere to JSF/WebLogic'),
-    (13, 3, 'Developed java functions for odmove.com and odfl.com Front-end'),
-    (14, 4, 'Custom setups and mapping EDI Data'),
-    (15, 4, 'Interact directly with customers for communications solutions'),
-    (16, 4, 'Migrate all EDI setups over to Extol software');
+    (1,  1, 'Building responsive user interfaces using Bootstrap and Flex'),
+    (2,  1, 'Added Angular unit testing to existing applications using Jasmine'),
+    (3,  1, 'Added Angular state management using NgRx (redux)'),
+    (4,  1, 'Working closely with UI/UX Design team to develop wireframes'),
+    (5,  1, 'Contributing member of the team that is responsible for building and maintaining the corporate Angular component set'),
+    (6,  1, 'Building rest API’s using .Net Core and Springboot'),
+    (7,  1, 'Leading a POC working with Raspberry Pi’s and creating Google Assistant Commands'),
+    (8,  2, 'Main point of contact for UI/UX development issues for entire IT dept'),
+    (9,  2, 'Developed first Angular front end as POC to demo to executive team'),
+    (10, 2, 'Built full custom Angular schematic for consistent app creation using corporate standards and styling'),
+    (11, 2, 'Building responsive user interfaces using Foundation and Bootstrap'),
+    (12, 2, 'Collaborated with diverse programming teams to prototype and solve complex business problems'),
+    (13, 2, 'Created containerized Docker front end UIs'),
+    (14, 3, 'Building responsive user interfaces using Foundation and Bootstrap'),
+    (15, 3, 'Production support including analyzing and repairing defects'),
+    (16, 3, 'Designed, developed, and tested software using Agile Scrum methodologies'),
+    (17, 3, 'Helped transition external website from JSP/WebSphere to JSF/WebLogic'),
+    (18, 3, 'Developed java functions using JSP and JSF'),
+    (19, 4, 'Custom setups and mapping EDI Data'),
+    (20, 4, 'Interact directly with customers for communications solutions');
